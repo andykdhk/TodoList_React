73bd877dfe8f2ea79045ece54101af74982fd394
Andy <andykdhk@gmail.com>
2022-02-25 23:34:56 -0700

    day1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
 import Header from "./component/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" />
+          <Route>{/* <EmptyPage /> */}</Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,9 +1,17 @@
 export default function Header() {
   return (
-    <div className="heaer">
+    <div className="header">
       <h1>
         <a href="/">Home</a>
       </h1>
+      <div className="menu">
+        <a href="#x" className="link">
+          word +
+        </a>
+        <a href="#x" className="link">
+          day +
+        </a>
+      </div>
     </div>
   );
 }
