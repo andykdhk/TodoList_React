7e7bbbd13b7db10eb8958120f92bc102528c163c
Andy <andykdhk@gmail.com>
2022-03-05 23:36:00 -0700

    next button working, but prev

--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,25 +1,45 @@
 import { Link } from "react-router-dom";
-import useFetch from "../hooks/useFetch";
+//import useFetch from "../hooks/useFetch";
 import { useEffect, useState } from "react";
 
 export default function DayList() {
   console.log("daylist page loaded");
   let [sortDays, setSort] = useState([]);
-  const days = useFetch("http://localhost:3001/days")
-    .map((day) => day.day)
-    .sort((a, b) => a - b);
-
+  const [days, setDays] = useState([]);
+  let current;
   useEffect(() => {
-    setSort(days);
-    console.log("SORTED:sss", sortDays);
+    fetch("http://localhost:3001/days")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setDays(data);
+      });
   }, []);
+
+  //let [days, setDay] = useState([]);
+  // const days = useFetch("http://localhost:3001/days")
+  //   .map((day) => day.day)
+  //   .sort((a, b) => a - b);
+
+  // useEffect(() => {
+  //   setSort(days);
+  //   console.log("SORTED:", sortDays);
+  // }, []);
   //let sortDays = days.map((day) => day.day).sort((a, b) => a - b);
-
-  function cutArr() {
-    setSort(sortDays.slice(1));
-    console.log("after button", sortDays);
+  function prevBtn() {
+    setDays(days.slice(-1));
   }
-
+  function nextBtn() {
+    if (days.length >= 4) {
+      //console.log(days.splice(0, 1));
+      // setDays(days.splice(0, 1));
+      setDays(days.slice(4));
+    } else {
+      alert("last page");
+    }
+  }
+  console.log(days.length);
   return (
     <div className="dayList">
       <h2>Daily task List</h2>
@@ -30,13 +50,13 @@ export default function DayList() {
 
       <div>
         <ul>
-          <button>Previous</button>
-          {sortDays.map((day, indexNum) => (
-            <li key={indexNum} className="day">
-              <Link to={`/day/${day}`}>Day {day}</Link>
+          <button onClick={prevBtn}>Previous</button>
+          {days.map((day) => (
+            <li key={day.id} className="day">
+              <Link to={`/day/${day.day}`}>Day {day.day}</Link>
             </li>
           ))}
-          <button onClick={cutArr}>Next</button>
+          <button onClick={nextBtn}>Next</button>
         </ul>
       </div>
     </div>
