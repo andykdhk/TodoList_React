36a4b5a7144edb172b98ff619801dbbca34e7e90
Andy <andykdhk@gmail.com>
2022-03-11 23:33:33 -0700

    1. dd asc, and descebutton. 2. Gap button has a bug.

--- src/component/DayList.js
+++ src/component/DayList.js
@@ -7,8 +7,12 @@ export default function DayList() {
 
   let [curPg, setCurrentPg] = useState(0);
   let [lasPg, setLastPg] = useState(4);
-  const gap = 4;
-
+  let [a, setA] = useState("a");
+  let [b, setB] = useState("b");
+  let [gap, setGap] = useState(4);
+  // const gap = 4;
+  let x = gap;
+  let [order, setOrder] = useState(true);
   const days = useFetch("http://localhost:3001/days");
 
   function prevBtn() {
@@ -31,20 +35,39 @@ export default function DayList() {
     }
   }
 
+  function sorting(a, b) {
+    if (order === true) {
+      return a - b;
+    } else if (order === false) {
+      return b - a;
+    }
+  }
+
+  function gapBtn() {
+    x = x + 1;
+    setGap(x);
+    setLastPg(x);
+  }
   return (
     <div className="dayList">
       <h2>Daily task List</h2>
-      {/* <div className="menu">
-        <button>Day Mode</button>
-        <button>D-day Mode</button>
-      </div> */}
+
+      <div className="menu">
+        <button onClick={() => setOrder(true)} className="ascOrder">
+          Day Mode
+        </button>
+        <button onClick={() => setOrder(false)} className="dscOrder">
+          D-day Mode
+        </button>
+        <button onClick={gapBtn}>gap: {gap}</button>
+      </div>
 
       <div>
         <ul>
           <button onClick={prevBtn}>Previous</button>
           {days
             .map((day) => day.day)
-            .sort((a, b) => a - b)
+            .sort((a, b) => sorting(a, b))
             .map((day, i) => (
               <li key={i} className="day">
                 <Link to={`/day/${day}`}>Day {day}</Link>
