c531996f1002b9a8b7a5ca13f1dcbf1ba1998eb4
Andy <andykdhk@gmail.com>
2022-03-17 23:12:02 -0600

    working params

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -1,7 +1,25 @@
 import { Link } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export default function Todo() {
-  return <div>hello</div>;
+  const { day } = useParams();
+
+  const tasks = useFetch('http://localhost:3001/tasks?day=${day}`)
+
+
+
+  return (
+    <div>
+      <div>hello</div>
+      <table>
+        <tbody>
+          {tasks.map((task) => (
+            <td>task.task</td>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
